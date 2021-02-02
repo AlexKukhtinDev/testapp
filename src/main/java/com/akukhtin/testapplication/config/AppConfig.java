efa21d4e928286f17e7df99507a1cf48ca94165d
Aleksander Kukhtin <kukhtin@radmirtech.com.ua>
2021-02-02 23:44:38 +0200

    added configs

---
+++ src/main/java/com/akukhtin/testapplication/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.akukhtin.testapplication.config;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class AppConfig {
+
+  private AppConfig() {
+  }
+
+  @Bean
+  public static PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer()
+  {
+    PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+    ppc.setLocation(new ClassPathResource("application.properties"));
+    ppc.setIgnoreUnresolvablePlaceholders(true);
+    return ppc;
+  }
+}
